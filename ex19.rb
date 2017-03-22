ff14169292eaaf537fef11bbbb73f086b8e32545
SLAScodes <lrose@slusd.us>
2017-03-22 16:24:00 -0700

    add ex 19 + variations

--- ex19.rb
+++ ex19.rb
@@ -3,7 +3,7 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers)
 	puts "You have #{cheese_count} cheeses!"
 	puts "You have #{boxes_of_crackers} boxes of crackers!"
 	puts "Wow! That's enough for a party!"
-	puts "Get a blanket.\n"
+	puts "Get a blanket.\n\n\n"
 end
 
 # ----RUN THE FUNCTION----
---
+++ ex19a.rb
@@ -0,0 +1,84 @@
+# def wardrobe contents and quantity
+def meal(fru_or_veg, grain, protein)
+	puts "\nBreakfast contained #{fru_or_veg} produce sources, #{grain} grain sources, and #{protein} protein sources. "
+	puts "That's a healthy breakfast!"
+end
+
+
+# Run the function
+
+# 1 ----------------value to function directly
+
+puts "\n>>Pass values to function directly"
+
+meal(3, 1, 1)
+
+puts "\n<<End passing of values to function directly\n\n"
+
+# 2 -----------------value to function via variables
+
+puts "\n>>Pass values to function via variables"
+
+carbs = 2
+grain_src = 1
+protein_src = 1
+
+meal(carbs, grain_src, protein_src)
+
+puts "\n>>End pass values to function via variables\n\n"
+
+# 3 ------------------value to function via math
+
+puts "\n>>Pass values to function via math"
+
+meal(2 * 2, 1 + 1, 10/2)
+
+puts "\n<<End pass values to function via math"
+
+# 4 -------------------value to function via variables + math
+
+puts "\n>>Pass values to function via math + variable. "
+
+meal(carbs + 1, grain_src + 1, protein_src + 2)
+puts "I'm full!!\n\n"
+
+puts "\n<<End pass values to function via math and variables"
+
+#5 -------------------Value to function directly + math + variable
+
+meal(1+1, grain_src + 1, 3)
+puts "\n\n"
+
+#6 -------------------Take input from user
+
+puts ">>START VARIATION 6\n\n"
+prompt = '> '
+
+puts "Greetings. Please tell us what you had for breakfast today.\n\n"
+
+puts "I had these many pieces of fruit:\n\n", prompt
+
+fruit = $stdin.gets.chomp
+
+
+puts "Toast, bread, or cereal? How many pieces or bowls?\n\n", prompt
+
+grain = $stdin.gets.chomp
+
+
+puts "...And how about protein...eggs, ham, etc. How many eggs or slices of whatever?\n\n", prompt
+
+protein = $stdin.gets.chomp
+
+
+meal(fruit, grain, protein)
+
+puts "<<END VARIATION 6\n\n"
+
+
+
+
+
+
+
+
